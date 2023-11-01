0cf3e1c76269f612d1e4bfa2f6cbad7470cf1b30
Rayan <rayanyassen23@gmail.com>
2023-11-01 11:38:01 +0200

    عمل على الشاشات

---
+++ app/src/main/java/RY/Yassen/finalapplaction/SignInActivty.java
@@ -0,0 +1,14 @@
+package RY.Yassen.finalapplaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_activty);
+    }
+}
