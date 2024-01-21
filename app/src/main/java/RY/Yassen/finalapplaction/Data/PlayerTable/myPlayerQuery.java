071dd9c49203e8a6b89d2620571fc84006179df3
Rayan <rayanyassen23@gmail.com>
2024-01-21 11:01:07 +0200

    Rayan

--- app/src/main/java/RY/Yassen/finalapplaction/Data/PlayerTable/myPlayerQuery.java
+++ app/src/main/java/RY/Yassen/finalapplaction/Data/PlayerTable/myPlayerQuery.java
@@ -1,7 +1,10 @@
 package RY.Yassen.finalapplaction.Data.PlayerTable;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -17,6 +20,28 @@ public interface myPlayerQuery {
      */
 @Query("Select * FROM myPlayer Where username=:name ")
     List <myPlayer> getUsername(String name);
+    /**
+     *  ادخال مهمات
+     * @param t  مجموعه مهمات
+     */
+    @Insert
+    void insertinfo(myPlayer...t); // ثلاثة نقاط تعني ادخال مجموعه
+
+    /**
+     * تعديل المعلومات
+     * @param tasks مجموعه لتعديل المهمات (التعديل حسب المفتاح الرئيسي)
+     */
+    @Update
+    void Updateinfo(myPlayer... tasks);
+
+    /**
+     * حذف  المعلومه
+     * @param tasks حذف المعلومات (حسب المفتاح الرئيسي)
+     */
+
+    @Delete
+    void deletTask(myPlayer... tasks);
+
 
 
 
