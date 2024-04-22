8a511faad6362acbbf88cd276a0f5b64e80c7b72
Rayan <rayanyassen23@gmail.com>
2024-04-22 13:26:45 +0300

    عمل على شاشه البروفايل

--- app/src/main/java/RY/Yassen/finalapplaction/Profile.java
+++ app/src/main/java/RY/Yassen/finalapplaction/Profile.java
@@ -33,15 +33,6 @@ public class Profile extends AppCompatActivity {
     private TextInputEditText ET_RUINCLUB;
     private AutoCompleteTextView autoetProfile;
 
-    //upload: 1 add Xml image view or button and upload button
-//upload: 2 add next fileds
-    private final int IMAGE_PICK_CODE=100;// קוד מזהה לבקשת בחירת תמונה
-    private final int PERMISSION_CODE=101;//קוד מזהה לבחירת הרשאת גישה לקבצים
-    private ImageButton imgBtnl;//כפתור/ לחצן לבחירת תמונה והצגתה
-    private Button btnUpload;// לחצן לביצוע העלאת התמונה
-    private Uri toUploadimageUri;// כתוב הקובץ(תמונה) שרוצים להעלות
-    private Uri downladuri;//כתובת הקוץ בענן אחרי ההעלאה
-
 
 
 
@@ -57,15 +48,7 @@ public class Profile extends AppCompatActivity {
         Et_phone = findViewById(R.id.Et_phone);
         ET_Username=findViewById(R.id.ET_Username);
         ET_RUINCLUB=findViewById(R.id.ET_RUINCLUB);
-        //upload: 3
-        imgBtnl=findViewById(R.id.imgBtn);
-        imgBtnl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-
-            }
-        });
 
 
     }
